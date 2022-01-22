af45eeebbec1b963cc91eb528a51a85a1e49c13c
Hrushikesh Vetagiri <vetagirih@gmail.com>
2022-01-22 22:52:32 +0530

    adding values from dict

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,10 +15,14 @@ export class AppComponent {
   varNameDict: any = {}
   newGroupDict: any ={}
   Object = Object;
+  dictValues: any = {}
 
   constructor(private renderer: Renderer2){
     this.tabListFunction();
     this.tabContentDisplayFunction();
+    this.createDictValues();
+
+    console.log(this.tabIdDict)
   }
 
   openCity(event: any, tabId:any){
@@ -34,8 +38,30 @@ export class AppComponent {
   }
   this.renderer.setStyle(document.getElementById(tabId), 'display', 'block');
   event.currentTarget.className += " active";
+
+  var SwitchId = event.target.getAttribute('data-myAttribute');
+    switch(SwitchId) {
+        case "tabId0":
+              this.fillDictValues()
+              console.log(this.tabIdDict)
+              break;
+        default:
+              break
+    }
   }
 
+  fillDictValues(){
+    for (var key in this.tabIdDict){
+      for(var group in this.tabIdDict[key]){
+        for(var v in this.tabIdDict[key][group]){
+          // if v in dictValues get the value and put it in v
+          if(v in this.dictValues){
+            this.tabIdDict[key][group][v] = this.dictValues[v]
+          }
+        }
+      }
+    }
+  }
 
   tabListFunction(){
     fetch('../assets/MPM10UI.xml').then((response) => {
@@ -44,7 +70,6 @@ export class AppComponent {
         var vars = parser.getElementsByTagName("tab");
         var count = 0;
         for(var i = 0; i < vars.length; i++){
-          
           let tabTag = {
             tabName : vars[i].getAttribute("name"),
             tabId : "tabId"+count
@@ -55,7 +80,6 @@ export class AppComponent {
       });
     });
   }
-
 
   tabContentDisplayFunction(){
     fetch('../assets/MPM10UI.xml').then((response) => {
@@ -79,7 +103,7 @@ export class AppComponent {
           }
           this.tabIdDict[String("tabId"+count++)] = this.groupNameDict
         }
-        console.log(this.tabIdDict)
+        // console.log(this.tabIdDict)
       });
     });
   }
@@ -88,5 +112,27 @@ export class AppComponent {
     return value === "" || typeof value === "string" || typeof value === "number";
   }
 
+  createDictValues(){
+    fetch('../assets/MPM10UI.xml').then((response) => {
+       response.text().then((xml) => {
+         const parser = new DOMParser().parseFromString(xml, "text/xml");
+         var tabId = parser.getElementsByTagName("tab");
+          var counter = 0;
+          for(var i = 0; i < tabId.length; i++){
+            for(var j = 0; j < tabId[i].children.length; j++){
+              for(var k =0; k < tabId[i].children[j].children.length; k++){
+                var text=tabId[i].children[j].children[k].getAttribute("name")
+                var result;
+                if(text != null){
+                  result = text.substring(8);
+                }
+                this.dictValues[String(result)] = counter++;
+              }
+            }
+          }
+          // console.log(this.dictValues)
+       });
+    });
+  }
 }
 
