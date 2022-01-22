ec269520decdc3affdc6279f9eed28aa873a5a9b
Hrushikesh Vetagiri <vetagirih@gmail.com>
2022-01-22 22:17:09 +0530

    displayed the content to tabs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,67 +1,92 @@
-import { Component } from '@angular/core';
+import { Component, Renderer2 } from '@angular/core';
+import {KeyValue} from '@angular/common';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   title = 'displayFrontEnd';
-  list: any = [];
   tabList:any = []
-  groupDict: any = {}
-  varlist : any = []
-  varValues : any = {}
+  tabIdDict: any = {}
+  groupNameDict: any = {}
+  varNameDict: any = {}
+  newGroupDict: any ={}
+  Object = Object;
 
-  constructor(){
+  constructor(private renderer: Renderer2){
     this.tabListFunction();
-    this.groupVarFunction();
-    console.log(this.tabList)
-    console.log(this.list) 
+    this.tabContentDisplayFunction();
   }
 
-
-  groupVarFunction(){
-    fetch('../assets/MPM10UI.xml').then((response) => {
-      response.text().then((xml) => {
-        const parser = new DOMParser().parseFromString(xml, "text/xml");
-        var vars = parser.getElementsByTagName("tab");
-        for(var i = 0; i < vars.length; i++){
-          for(var j = 0; j < vars[i].children.length; j++){
-            this.groupDict = {
-                name: vars[i].children[j].getAttribute("name"),
-                varDict: 2
-            }
-            this.varlist = [];
-            this.varValues = {}
-            for(var k = 0; k < vars[i].children[j].children.length; k++){
-              var text=vars[i].children[j].children[k].getAttribute("name")
-              var result;
-              if(text != null){
-                result = text.substring(8);
-              }
-              this.varValues[String(result)] = null
-            }
-              this.varlist.push(this.varValues)
-            this.groupDict['varDict'] = this.varlist
-            this.list.push(this.groupDict)
-          }
-        }
-        // console.log(this.list)
-      });
-    });
+  openCity(event: any, tabId:any){
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+    // tabcontent[i].style.display = "none";
+    this.renderer.setStyle(tabcontent[i], 'display', 'none');
   }
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  this.renderer.setStyle(document.getElementById(tabId), 'display', 'block');
+  event.currentTarget.className += " active";
+  }
+
 
   tabListFunction(){
     fetch('../assets/MPM10UI.xml').then((response) => {
       response.text().then((xml) => {
         const parser = new DOMParser().parseFromString(xml, "text/xml");
         var vars = parser.getElementsByTagName("tab");
+        var count = 0;
         for(var i = 0; i < vars.length; i++){
-            this.tabList.push(vars[i].getAttribute("name"))
+          
+          let tabTag = {
+            tabName : vars[i].getAttribute("name"),
+            tabId : "tabId"+count
+          }
+            this.tabList.push(tabTag)
+            count++;
         }
-        // console.log(this.tabList)
       });
     });
   }
+
+
+  tabContentDisplayFunction(){
+    fetch('../assets/MPM10UI.xml').then((response) => {
+      response.text().then((xml) => {
+        const parser = new DOMParser().parseFromString(xml, "text/xml");
+        var tabId = parser.getElementsByTagName("tab");
+        var count = 0;
+        for(var i = 0; i < tabId.length; i++){
+          this.groupNameDict = {}
+          for(var j = 0; j < tabId[i].children.length; j++){
+            this.varNameDict = {}
+            for(var k = 0; k < tabId[i].children[j].children.length; k++){
+              var text=tabId[i].children[j].children[k].getAttribute("name")
+              var result;
+              if(text != null){
+                result = text.substring(8);
+              }
+              this.varNameDict[String(result)] = null
+            }
+            this.groupNameDict[String(tabId[i].children[j].getAttribute("name"))] = this.varNameDict
+          }
+          this.tabIdDict[String("tabId"+count++)] = this.groupNameDict
+        }
+        console.log(this.tabIdDict)
+      });
+    });
+  }
+
+  isHtmlPrintable(value:any){
+    return value === "" || typeof value === "string" || typeof value === "number";
+  }
+
 }
+
